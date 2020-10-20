7783842ed9ac68061cecf1135324e1769a15506a
Petar Pribetić <pribetic.petar@gmail.com>
2020-10-20 13:42:23 +0200

    initial commit, register in progress

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  // const [loggedUser, setLoggedUser] = useState("");
+  // const 
+
+  return (
+    <div>
+      App.js
+    </div>
+  );
+}
+
+export default App;
